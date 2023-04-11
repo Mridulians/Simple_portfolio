33be62774f99b21c9de7b9c1ec3051275c2e88ce
Mridulians <gmridul898@gmail.com>
2023-04-11 19:31:15 +0530

    initial commit

---
+++ data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+      id: 1,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+    {
+      id: 2,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+    {
+      id: 3,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+    {
+      id: 4,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+    {
+      id: 5,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+    {
+      id: 6,
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsLdD_ktaiotvjzo1EHxOIerHyKza6AKP6pw&usqp=CAU",
+      link: "https://mridulians.github.io/pizzaHub.github.io/",
+    },
+  ];
